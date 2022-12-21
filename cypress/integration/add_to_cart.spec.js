ef0c2aa4c93a01c6719fe5bd806d610905443b9e
js2oh <js2oh@uwaterloo.ca>
2022-12-21 09:10:22 -0500

    Cypress: add to cart tested

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/products')
+  });
+
+  it("Add to cart by clicking on an add button", () => {
+    cy.get('[data-bs-toggle="dropdown"]')
+      .last()
+      .click()
+      .next()
+      .children()
+      .first()
+      .find("a.nav-link")
+      .as('cart')
+      .then(($link) => {
+        const count = $link.text().split(/[\(\)]/)[1]
+        cy.wrap(count).as('count')
+      })
+
+    cy.get(".products article")
+      .should("be.visible")
+      .first()
+      .find("div button.btn")
+      .click({ force: true })
+
+    cy.get('@count')
+      .then((count) => {
+        cy.get('@cart')
+          .should('include.text', `${Number(count) + 1}`)
+      })
+  });
+})
--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,10 +1,3 @@
-// Navigate from the home page to the product detail page by click on a product.
-
-// Start with a copy of all the setip code from the home_page feature.
-// Make sure you start by visiting the home page
-// and then click one of the product partials in order to navigate directly to a product details page.
-// Refer to the code in the _product.html.erb partial to determine how you are going to select the link to click.
-
 /// <reference types="cypress" />
 
 describe('example to-do app', () => {
