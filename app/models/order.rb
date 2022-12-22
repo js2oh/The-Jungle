8e197176d8a5519e64add7f225149452bad9b3bf
js2oh <js2oh@uwaterloo.ca>
2022-12-22 01:15:48 -0500

    RSpec Inventory Adjustment

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,19 @@
 class Order < ApplicationRecord
-  
+
   has_many :line_items
 
   monetize :total_cents, numericality: true
 
   validates :stripe_charge_id, presence: true
 
+  after_create :deduct_inventory
+
+  private
+
+  def deduct_inventory
+    self.line_items.each do |line_item|
+      line_item.product.quantity -= line_item.quantity
+      line_item.product.save!
+    end
+  end
 end
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  describe 'After Creation' do
+    before :each do
+      @quantity1 = 9
+      @quantity2 = 10
+      @category = Category.find_or_create_by! name: "Spec Category"
+      # Setup at least two products with different quantities, names, etc.
+      @product1 = Product.create!(
+        name: "Product 1",
+        category_id: @category.id,
+        quantity: @quantity1,
+        price: 99.99
+      )
+      @product2 = Product.create!(
+        name: "Product 2",
+        category_id: @category.id,
+        quantity: @quantity2,
+        price: 10.10
+      )
+      # Setup at least one product that will NOT be in the order
+      @product = Product.create!(
+        name: "Not in Order",
+        category_id: @category.id,
+        quantity: 77,
+        price: 77.77
+      )
+    end
+    # pending test 1
+    it 'deducts quantity from products based on their line item quantities' do
+      # TODO: Implement based on hints below
+      # 1. initialize order with necessary fields (see orders_controllers, schema, and model definition for what is required)
+      @order = Order.new(
+        email: "canadaGoose@gmail.com",
+        total_cents: 110.09,
+        stripe_charge_id: "stripe_charge_id",
+      )
+      # 2. build line items on @order
+      cart = [@product1, @product2].map {|product| { product: product, quantity: 3 } }
+      cart.each do |entry|
+        product = entry[:product]
+        quantity = entry[:quantity]
+        @order.line_items.new(
+          product: product,
+          quantity: quantity,
+          item_price: product.price,
+          total_price: product.price * quantity
+        )
+      end
+      # 3. save! the order - ie raise an exception if it fails (not expected)
+      @order.save!
+      # 4. reload products to have their updated quantities
+      @product1.reload
+      @product2.reload
+      # 4. use RSpec expect syntax to assert their new quantity values
+      expect(@product1.quantity).to eq(@quantity1 - 3)
+      expect(@product2.quantity).to eq(@quantity2 - 3)
+      @order.line_items.each do |line_item|
+        line_item.destroy
+      end
+      @order.destroy
+    end
+    # pending test 2
+    xit 'does not deduct quantity from products that are not in the order' do
+      # TODO: Implement based on hints in previous test
+    end
+  end
+end
